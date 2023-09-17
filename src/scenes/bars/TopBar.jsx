dee2ca1a990d295a7fd289dbd2c4881ba894c2bb
ademola <ademolaowoeye07@gmail.com>
2023-09-17 09:01:54 +0100

    udated light dark toggle

--- src/scenes/bars/TopBar.jsx
+++ src/scenes/bars/TopBar.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, IconButton, useTheme } from "@mui/material";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import { colorTokens, globalColorContext } from '../../theme'
+import { ColorModeContext, tokens } from '../../theme'
 
 const Topbar = () => {
-    const colorMode = React.useContext(globalColorContext)
+    const colorMode = React.useContext(ColorModeContext)
     const theme = useTheme()
-    // const color = colorTokens(theme.palette.mode)
+    const colors = tokens(theme.palette.mode);
+
+    const [icon, setIcon] = useState(false)
+    const switchIcon = () => {setIcon(!icon)}
+
+    const toggle_Theme_icon = () => {
+        colorMode.toggleColorMode()
+        switchIcon()
+    }
 
     return (
-        <Box display="flex" justifyContent="flex-end">
+        <Box display="flex" justifyContent="flex-end" variant="contained">
 
-          <IconButton onClick={colorMode.toggleColorMode}>
-               {theme.palette.mode === "dark" ? ( <DarkModeOutlinedIcon /> ) : ( <LightModeOutlinedIcon /> )}
+          <IconButton onClick={toggle_Theme_icon}>
+              {icon ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon /> }     
           </IconButton>
 
-            <Box  display="flex"  >
+            <Box display="flex">
                 <IconButton> < NotificationsOutlinedIcon /> </IconButton>
                 <IconButton> < SettingsOutlinedIcon /> </IconButton>
                 <IconButton> < PersonOutlinedIcon /> </IconButton>
