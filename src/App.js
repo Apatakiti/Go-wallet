78abd6fd9be33896397c6179926f84cfa86e815b
ademola <ademolaowoeye07@gmail.com>
2023-09-17 08:59:58 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-// import React, { useState } from 'react'
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import { ThemeProvider } from '@mui/material/styles';
 import  CssBaseline   from '@mui/material/CssBaseline';
-import { globalColorContext, useColorMode } from "./theme";
-import SideBar from '../src/scenes/bars/sidebar'
-import Topbar from '../src/scenes/bars/TopBar'
-import Button from '@mui/material/Button';
+import SideBar from '../src/scenes/bars/sidebar';
+import Topbar from '../src/scenes/bars/TopBar';
+import { ColorModeContext, useMode } from "./theme";
+
+import DashBoard from "./scenes/dashboard/Dashboard";
+import MarketOverview from "./scenes/marketOverview/marketOverview";
+import Exchange from "./scenes/exchange/exchange";
+import SendRecieve from "./scenes/sendRecieve/sendRecieve";
+import Transactions from "./scenes/Transactions/Transactions";
+import HelpSupport from "./scenes/helpSupport/helpSupport";
 
 function App() {
-  const [theme, colorState] = useColorMode()
-
+const [themeMode, colorMode] = useMode()
   return ( 
     <ProSidebarProvider>
-     <globalColorContext.Provider value={colorState} >
-      <ThemeProvider theme={theme} >
+     <ColorModeContext.Provider value={colorMode} >
+      <ThemeProvider theme={themeMode} >
         <div id="app" style={{ display:"flex" }} >
             <CssBaseline />
             <SideBar />
+
             <main style={{flexBasis:"100%"}}>
               <Topbar />
-              <h1 style={{ color: "green", textAlign:"center" }}> React-Pro-Sidebar </h1>
-              <Button variant="contained" color="secondary"> Click me </Button>
+
+              <Routes>
+                <Route path="/" element={<DashBoard />} />
+                <Route path="/MarketOverview" element={<MarketOverview />} />
+                <Route path="/Exchange" element={<Exchange />} />
+                <Route path="/SendRecieve" element={<SendRecieve />} />
+                <Route path="/Transactions" element={<Transactions />} />
+                <Route path="/HelpSupport" element={<HelpSupport />} />
+            </Routes>
             </main>
           </div>
        </ThemeProvider>
-     </globalColorContext.Provider>
+     </ColorModeContext.Provider>
 </ProSidebarProvider>
   );
 }
