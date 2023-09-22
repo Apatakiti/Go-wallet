ff5cf47532055aac8851359992f5bbc0ca1eafea
ademola <ademolaowoeye07@gmail.com>
2023-09-22 18:16:43 +0100

    added AsyncThunk for Api request and extraReducers

--- src/Redux/slice.js
+++ src/Redux/slice.js
@@ -1,20 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { fetchCryptoData } from "./getApiData";
 
-const initialState = { 
-    value: localStorage.getItem('savedBalance') || 0,
-};
+export const fetchCryptoDataAsync = createAsyncThunk(
+  "allData/fetchCryptoDataAsync",
+  async (_, thunkAPI) => {
+    try {
+      // thunkAPI.dispatch(action loading )
+      const data = await fetchCryptoData();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-const balanceSlice = createSlice({
-  name: 'balance',
-  initialState,
+const allDataSlice = createSlice({
+  name: "allData",
+  initialState: {
+    coinData: [],
+    loading: false,
+    error: null,
+    balance: localStorage.getItem("savedBalance") || 0,
+  },
   reducers: {
     saveBalance: (state, action) => {
-      state.value = action.payload;
-      localStorage.setItem('savedBalance', action.payload);
+      state.balance = action.payload;
+      localStorage.setItem("savedBalance", action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCryptoDataAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.coinData = action.payload;
+      })
+      .addCase(fetchCryptoDataAsync.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchCryptoDataAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
 
-export const { saveBalance } = balanceSlice.actions;
+// needed for external value dispatch
+export const { saveBalance } = allDataSlice.actions;
 
-export default balanceSlice.reducer;
+export default allDataSlice.reducer;
