2ea4625c40ce029639874db5e90fe8988bdc80aa
ademola <ademolaowoeye07@gmail.com>
2023-09-22 18:13:11 +0100

    Api request file

---
+++ src/Redux/getApiData.js
@@ -0,0 +1,36 @@
+const apiUrl = "https://api.coingecko.com/api/v3/coins/markets";
+
+export const fetchCryptoData = async () => {
+  const params = new URLSearchParams({
+    vs_currency: "usd",
+    order: "volume_desc",
+    per_page: 100,
+    page: 1,
+  });
+
+  const fullUrl = `${apiUrl}?${params.toString()}`;
+
+  try {
+    const response = await fetch(fullUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data.map((crypto, index) => ({
+      Id: `${index + 1}`,
+      Name: {
+        name: `${crypto.name}`,
+        image: `${crypto.image}`,
+        symbol: `${crypto.symbol}`,
+      },
+      Price: `$${crypto.current_price}`,
+      MarketCap: `$${crypto.market_cap}`,
+      a24hrVolume: `$${crypto.total_volume}`,
+    }));
+  } catch (error) {
+    throw error;
+  }
+};
