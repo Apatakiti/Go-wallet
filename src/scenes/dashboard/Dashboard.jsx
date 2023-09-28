52be1ed9da22874abab219ae60ee1fdee541865a
ademola <ademolaowoeye07@gmail.com>
2023-09-28 21:26:23 +0100

    updated

--- src/scenes/dashboard/Dashboard.jsx
+++ src/scenes/dashboard/Dashboard.jsx
@@ -1,22 +1,32 @@
-import Button from "@mui/material/Button"
+// import Button from "@mui/material/Button"
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { saveBalance } from '../../Redux/slice';
+import { useSelector } from 'react-redux';
 import { tokens } from "../../theme";
 import { useTheme, Box, Typography } from "@mui/material";
 import Header from "../../components/header";
+
+const coinList = [
+    { name: 'Bitcoin' },
+    { name: 'Ethereum' },
+    { name: 'Ripple' },
+    { name: 'Litecoin' },
+    { name: 'Stellar' },
+    { name: 'Cardano' },
+  ];
+
 
 const DashBoard = () => {
      const theme = useTheme()
      const colors = tokens(theme.palette.mode)
 
-     const balance = useSelector((state) => state.data.balance);
-     const dispatch = useDispatch();
+     const coinBalances = useSelector((state) => state.data.allCoinBalance);
+     const CoinHoldings = coinList.filter((coin) => coinBalances[coin.name] > 0);
+    
 
-     const handleSaveNumber = () => {
-       const randomNumber = Math.floor(Math.random() * 100);
-       dispatch(saveBalance(randomNumber));
-      };
+    //  const handleSaveNumber = () => {
+    //    const randomNumber = Math.floor(Math.random() * 100);
+    //    dispatch(saveBalance(randomNumber));
+    //   };
 
     return ( 
         <Box  padding={"0 2% 0 2%"}>
@@ -29,27 +39,36 @@ const DashBoard = () => {
             <Box padding={"8px"} backgroundColor={colors.primary[400]} borderRadius={"10px"}
             >
                 <Header text={"Coin holding Balance" } />
-                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> BitCoin : 0.00{balance} </Typography>
+                {/* <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> uuuuuuuuumml </Typography>
                 <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }} > Etherum : {balance} </Typography>
-                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }} > USDT : 23{balance} </Typography>
-                <Button variant="contained" color="primary" onClick={handleSaveNumber}>Saved Balance</Button>
+                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }} > USDT : 23{balance} </Typography> */}
+                {/* <Button variant="contained" color="primary" onClick={handleSaveNumber}>Saved Balance</Button> */}
+                <div>
+
+<h1>Coin Balances</h1>
+{CoinHoldings.map((coin) => (
+  <div key={coin.name}>
+    <p>{coin.name}: {coinBalances[coin.name]}</p>
+  </div>
+))}
+</div>
             </Box>
 
             <Box margin={"0 0 0 2%"} display={"flex"} flexDirection={"row"} justifyContent={"center"} flexBasis={"40%"}>
                 <Box  borderRadius={"10px"} padding={"8px"} backgroundColor={colors.primary[400]}>
                 <Header text={"Coin holding MarketCap"} />
-                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> BitCoin : {balance} </Typography>
-                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Etherum : {balance} </Typography>
+                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> BitCoin :  </Typography>
+                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Etherum :  </Typography>
                 </Box>
             </Box>
 
             <Box margin={"0 0 0 2%"} borderRadius={"10px"} padding={"8px"} backgroundColor={colors.primary[400]}>
             <Header text={"Portfolio Net Value"} />
-                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> BitCoin : {balance} </Typography>
-                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Etherum : {balance} </Typography>
+                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> BitCoin :</Typography>
+                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Etherum : </Typography>
                 <Box display={"flex"}>
                     <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Total Value : </Typography> 
-                 <Typography variant="h5" color={"green"} fontSize={"25px"} marginLeft={"5px"} >  ${balance}
+                 <Typography variant="h5" color={"green"} fontSize={"25px"} marginLeft={"5px"} >  $
                  </Typography>
                  </Box>
             </Box>
@@ -61,17 +80,17 @@ const DashBoard = () => {
 
             <Box padding={"8px"} backgroundColor={colors.primary[400]} borderRadius={"10px"} >
                 <Header text={"Portfolio pie chart stat"} />
-                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> BitCoin : {balance} </Typography>
-                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Etherum : {balance} </Typography>
-                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> USDT : {balance} </Typography>      
+                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> BitCoin :</Typography>
+                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Etherum :</Typography>
+                <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> USDT :  </Typography>      
             </Box>
 
             <Box margin={"0 0 0 2%"} display={"flex"} flexDirection={"row"} justifyContent={"center"} flexBasis={"40%"}>
 
                 <Box borderRadius={"10px"} padding={"8px"} backgroundColor={colors.primary[400]}>
                 <Header text={"Coin holding trends"} />
-                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Table eow 1 BitCoin : {balance} </Typography>
-                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Table row 2 Etherum : {balance} </Typography>
+                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Table eow 1 BitCoin : </Typography>
+                    <Typography variant="h5" color={colors.greenAccent[400]} fontSize={"25px"} sx={{ m: "0 0 1px 0" }}> Table row 2 Etherum :  </Typography>
                 </Box>
             </Box>
 
